8cd6af5f6ea379d5a59c5ad0b00d73aefce4206f
Samira Amorim <s.amorim.p@gmail.com>
2019-03-23 10:32:13 -0400

    [task-29] - Informar maior valor - Samira

--- src/main/java/br/com/tdstecnologia/samira/Task29.java
+++ src/main/java/br/com/tdstecnologia/samira/Task29.java
@@ -9,22 +9,21 @@ package br.com.tdstecnologia.samira;
  *
  * @author Aluno1
  */
+import java.util.Objects;
 import javax.swing.JOptionPane;
-        
+
 public class Task29 {
-    
-    public static void main(String[] args){
-    String x = JOptionPane.showInputDialog("Adicione o valor X");
-    String y = JOptionPane.showInputDialog("Adicione o valor Y");
-    Integer x1 = Integer.valueOf(x);
-    Integer y1 = Integer.valueOf(y);
-    if (x1 > y1) {
+
+    public static void main(String[] args) {
+        String x = JOptionPane.showInputDialog("Adicione o valor X");
+        String y = JOptionPane.showInputDialog("Adicione o valor Y");
+        Integer x1 = Integer.valueOf(x);
+        Integer y1 = Integer.valueOf(y);
+        if (x1 > y1) {
             JOptionPane.showMessageDialog(null, "X é maior que Y");
-            if (x1 == y1) {
+            if (Objects.equals(x1, y1)) {
                 JOptionPane.showMessageDialog(null, "X é igual à Y");
             }
-        } }
-
+        }
+    }
 }
-
-
