90e0d6c4deef1b6f7758acfc251c659bf9593b22
Breno <breno.lu_arruda@hotmail.com>
2019-03-23 10:13:13 -0400

    [task-1] - Informar maior valor - Breno

---
+++ src/main/java/br/com/tdstecnologia/breno/MaiorValor.java
@@ -0,0 +1,34 @@
+package br.com.tdstecnologia.breno;
+
+import javax.swing.JOptionPane;
+
+public class MaiorValor 
+{
+    public static void main(String[] args) 
+    {
+        String valor1 = JOptionPane.showInputDialog(null, "Insira um número: ", "Maior valor", JOptionPane.WARNING_MESSAGE  );
+        Integer x = Integer.valueOf(valor1);
+        System.out.println("Valor digitado: " + x);
+        
+        String valor2 = JOptionPane.showInputDialog(null, "Insira outro número: ", "Maior valor", JOptionPane.WARNING_MESSAGE  );
+        Integer y = Integer.valueOf(valor2);
+        System.out.println("Valor digitado: " + y);
+        
+        if (x > y)
+        {
+            JOptionPane.showMessageDialog(null, x + " é maior que " + y);
+            System.out.println(x + " é maior que " + y);                        
+        }
+        else if (x < y)
+            {
+            JOptionPane.showMessageDialog(null, x + " é menor que " + y);
+            System.out.println(x + " é menor que " + y);            
+            }
+            else if (x == y)
+                {
+                JOptionPane.showMessageDialog(null, x + " é igual " + y);
+                System.out.println(x + " é igual " + y);                
+                }        
+    }       
+          
+}
