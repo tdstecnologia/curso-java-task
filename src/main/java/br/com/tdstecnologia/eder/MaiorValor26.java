413c2e012581b1b12c4e431acb7243c0011663e6
Éder Afonso Rabelo <ederafonsor@gmail.com>
2019-03-23 09:38:27 -0400

    [task-26] - Informar maior valor - Eder (#47)
    * [task-26] - Informar maior valor - Eder
    
    * [task-26] Corrigindo nome da classe

---
+++ src/main/java/br/com/tdstecnologia/eder/MaiorValor26.java
@@ -0,0 +1,27 @@
+package br.com.tdstecnologia.eder;
+
+import javax.swing.JOptionPane;
+
+public class MaiorValor26 {
+
+    /*
+    Informar o maior valor
+    
+    Faça um sistema que verifique qual o maior valor de 2 números informados e apresente a mensagem
+    x maior y
+    x igual y */
+    
+    public static void main(String[] args) {
+        String x = JOptionPane.showInputDialog("Adicione o valor X");
+        String y = JOptionPane.showInputDialog("Adicione o valor Y");
+        Integer x1 = Integer.valueOf(x);
+        Integer y1 = Integer.valueOf(y);
+        if (x1 > y1) {
+            JOptionPane.showMessageDialog(null, "X é maior que Y");
+            if (x1 == y1) {
+                JOptionPane.showMessageDialog(null, "X é igual à Y");
+            }
+        }
+    }
+
+}
