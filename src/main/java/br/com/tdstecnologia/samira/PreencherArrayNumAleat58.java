6b2c87b3f88d5c0794e1c52261daca4b804ea227
Samira Amorim <s.amorim.p@gmail.com>
2019-03-23 14:47:03 -0400

    Crie um array com 10 posições - Samira -58

---
+++ src/main/java/br/com/tdstecnologia/samira/PreencherArrayNumAleat58.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tdstecnologia.samira;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Aluno1
+ */
+public class PreencherArrayNumAleat58 {
+
+    /*Crie um array com 10 posições e preencha com números aleatórios até 100 
+     */
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        Random gerador=new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array [i] = gerador.nextInt(100);
+        }
+        for (int i = 0;i < array.length; i++) {
+        System.out.println(array [i]);
+        
+        }     
+       
+
+    }
+}
