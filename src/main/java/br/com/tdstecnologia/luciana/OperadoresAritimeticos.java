4a9a3623273939cc4a88e15257abd6a3d5e5ffc1
luciana Estevam <luestevam@hotmail.com>
2019-03-28 15:43:34 -0400

    operadores java

---
+++ src/main/java/br/com/tdstecnologia/luciana/OperadoresAritimeticos.java
@@ -0,0 +1,15 @@
+
+package br.com.tdstecnologia.luciana;
+
+public class OperadoresAritimeticos {
+
+    
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é igual a: " + m);
+        
+    }
+    
+}
